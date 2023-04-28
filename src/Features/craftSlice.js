436530b29e60b68b55a987cdafe63688a6bce4f7
paula cavender <you@example.com>
2023-04-28 17:08:07 +0100

    addProject reducer added

--- src/Features/craftSlice.js
+++ src/Features/craftSlice.js
@@ -15,7 +15,22 @@ const initialState = [
 export const craftSlice = createSlice({
   name: "craft",
   initialState,
-  reducers: {},
+  reducers: {
+    addProject: (state, action) => {
+      const newProject = {
+        id: Math.random() * 1000,
+        name: action.payload.name,
+        type: action.payload.type,
+        description: action.payload.description,
+        completed: false,
+        modal: false,
+        counter: 0,
+      };
+      state.push(newProject);
+    },
+  },
 });
+
+export const { addProject } = craftSlice.actions;
 
 export default craftSlice.reducer;
